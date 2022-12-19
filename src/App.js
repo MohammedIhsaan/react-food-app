6bd942d372cda42d2c5f7f4cf1f635595fbe6f5e
Mohammed Ihsaanul haque <mohammedihsaanulhaque@Mohammeds-MacBook-Air.local>
2022-12-19 18:37:15 +0530

    cart and modal is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+  const showCartHandler = () => {
+    setShowCart(true);
+  };
+  const hideCartHandler = () => {
+    setShowCart(false);
+  };
   return (
     <>
-      <Header />
-      <Meals />
+      {showCart && <Cart hideCartHandler={hideCartHandler} />}
+      <Header showCartHandler={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
